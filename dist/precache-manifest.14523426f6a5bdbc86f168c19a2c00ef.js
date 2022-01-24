53a3f664de0918bf99ad77a7fea917b8b31c63d8
Yura Korol <yrakorol200012@gmail.com>
2022-01-24 13:16:08 +0200

    install express and commit dist

---
+++ dist/precache-manifest.14523426f6a5bdbc86f168c19a2c00ef.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2a14b572906ceeb1c8e9",
+    "url": "/css/app.ab790f97.css"
+  },
+  {
+    "revision": "0ddbb267485c3f055c38",
+    "url": "/css/chunk-vendors.0cf80912.css"
+  },
+  {
+    "revision": "46fb71dba67f3bacc76feb88b1939916",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2a14b572906ceeb1c8e9",
+    "url": "/js/app.4f8632fb.js"
+  },
+  {
+    "revision": "0ddbb267485c3f055c38",
+    "url": "/js/chunk-vendors.5c28bc2a.js"
+  },
+  {
+    "revision": "0c5ef8887efff83ccdf4baff4d91e4c6",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
